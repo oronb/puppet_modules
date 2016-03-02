e39b31190a59d3744fbd8bda4ed96be15e5de254
oronb <orong1234@gmail>
2016-03-02 11:49:36 +0200

    n

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,58 @@
-#init.pp
+# Class: artifactory
+#
+# This module downloads Maven Artifacts from Artifactory
+#
+# Parameters:
+# [*url*] : The Artifactory base url (mandatory)
+# [*username*] : The username used to connect to artifactory
+# [*password*] : The password used to connect to artifactory
+#
+# Actions:
+# Checks and intialized the Artifactory support.
+#
+# Sample Usage:
+#  class artifactory {
+#   url      => 'http://artifactory.domain.com:8081',
+#   username => 'user',
+#   password => 'password',
+# }
+#
+class artifactory (
+  $url = 'http://oron-puppet-client-1:8081/artifactory',
+  $username = 'admin',
+  $password = 'password')
+{
 
-class cluster (
-        $type                   = undef,
-        $master                 = undef,
-        $depended               = undef,
-) {
- file {'/tmp/test-puppet':
- ensure => present,
- content => "$type $master $depended",
- }
+  # Check arguments
+  # url mandatory
+  if $url == '' {
+    fail('Cannot initialize the Artifactory class - the url parameter is mandatory')
+  }
+  $artifactory_url = $url
+
+  if ($username != '') and ($password == '') {
+    fail('Cannot initialize the Artifactory class - both username and password must be set')
+  } elsif ($username == '') and ($password != '') {
+    fail('Cannot initialize the Artifactory class - both username and password must be set')
+  } elsif ($username == '') and ($password == '') {
+    $authentication = false
+  } else {
+    $authentication = true
+    $user = $username
+    $pwd = $password
+  }
+
+  # Install script
+ # file { '/opt/artifactory-script/download-artifact-from-artifactory.sh':
+ #   ensure   => file,
+ #   owner    => 'root',
+ #   mode     => '0755',
+ #   source   => 'puppet:///modules/artifactory/download-artifact-from-artifactory.sh',
+ #   require  => File ['/opt/artifactory-script']
+ # }
+
+  file { '/opt/artifactory-script':
+    ensure => directory
+  }
+	class { 'artifactory::artifact': } 
 }
