7e0e6d02cef5425914dfa9a4e580682caf53fdab
oronb <orong1234@gmail.com>
2016-03-08 11:07:40 +0200

    Update download_artifactory.sh

--- files/download_artifactory.sh
+++ files/download_artifactory.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+set -e
 url=$1
 repository=$2
 output=$3
@@ -8,9 +9,5 @@ jenkins_build=$6
 artifact_type=$7
 filename=`cat /$output/$6`
 apache_path=$8
-echo "/usr/bin/curl -sS -f -L $url/$repository/$filename -o $output/$filename -u $user:$password -v --location-trusted"
+
 /usr/bin/curl -sS -f -L $url/$repository/$filename -o $output/$filename -u $user:$password -v --location-trusted
-#if [ $artifact_type == 'zip' ]
-#then
-#	/usr/bin/unzip -od ${apache_path} ${output}/${filename}.erb
-#fi
